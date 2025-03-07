ae3e8a689de3da53ce357ba030ebd01f2912f372
Patrik Ferreira <patrik.andradef@gmail.com>
2025-03-07 21:45:40 +0000

    Added API methods

--- server.js
+++ server.js
@@ -1,17 +1,86 @@
 import express from 'express';
+import { PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient()
 
 const app = express();
 app.use(express.json());
 
-const users = [];
-
-app.post('/users', (req, res) => {
-    users.push(req.body);
-    res.status(201).json(req.body);
+app.get('/users', async (req, res) => {
+    try {
+        const users = await prisma.user.findMany();
+        res.status(200).json(users);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: 'Internal server error' });
+    }
 });
 
-app.get('/users', (req, res) => {
-    res.status(200).json(users);
+app.get('/users/:id', async (req, res) => {
+    try {
+        const user = await prisma.user.findUnique({
+            where: {
+                id: req.params.id,
+            },
+        });
+        if (user) {
+            res.status(200).json(user);
+        } else {
+            res.status(404).json({ error: 'User not found' });
+        }
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: 'Internal server error' });
+    }
+})
+
+app.post('/users', async (req, res) => {
+    try {
+        const user = await prisma.user.create({
+            data: {
+                name: req.body.name,
+                email: req.body.email,
+                age: req.body.age,
+            },
+        });
+        res.status(201).json(user);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: 'Internal server error' });
+    }
+});
+
+app.put('/users/:id', async (req, res) => {
+    try {
+        const user = await prisma.user.update({
+            where: {
+                id: req.params.id,
+            },
+            data: {
+                name: req.body.name,
+                email: req.body.email,
+                age: req.body.age,
+            },
+        });
+        res.status(200).json(user);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: 'Internal server error' });
+    }
+});
+
+app.delete('/users/:id', async (req, res) => {
+    try {
+        await prisma.user.delete({
+            where: {
+                id: req.params.id,
+            },
+        });
+        res.status(204).send();
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: 'Internal server error' });
+    }
 });
 
 app.listen(3000, () => {
